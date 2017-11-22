60e7c84a3f12960b9d293c9129ab6d069323dd39
lw246 <leewilson246@gmail.com>
2017-11-22 14:30:25 +0000

    Added prettier reports to make me smile :-)

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -2,7 +2,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.*;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/feature-files"})
+@CucumberOptions(features = {"src/test/feature-files"},
+        format = {"pretty",
+                "json:target/cucumber.json",
+                "html:target/site/cucumber-pretty"},
+        tags = {"~@ignore"})
 
 public class CucumberRunner {
 }
